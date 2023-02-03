bd5e3b25cee349b5fb5a6ec64fb051baa757a8b6
Graydon von Gossler <graydonvg@gmail.com>
2023-02-03 15:47:37 +0200

    improved security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,40 @@
+const handleApiCall = (req, res) => {
+  const { input } = req.body;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: '',
+      app_id: '',
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: input,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: ``,
+    },
+    body: raw,
+  };
+
+  fetch(
+    `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json('Error with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -10,4 +47,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall,
 };
